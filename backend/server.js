c4a6ff8afbce4295f540b37ce42ba32da33c3edc
Shao Tan <t.shaoming@gmail.com>
2022-09-22 17:49:30 +0800

    Move files into backend folder

---
+++ backend/server.js
@@ -0,0 +1,5 @@
+const app = require('./app');
+
+app.listen(8000, () =>
+  console.log('Start server on port 8000!'),
+);
--- server.js
+++
@@ -1,5 +0,0 @@
-const app = require('./app');
-
-app.listen(3000, () =>
-  console.log('Start server on port 3000!'),
-);
